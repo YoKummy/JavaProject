4019ca291c57743d1374c27eab15be59f19e86bd
YoKummy <80161555+YoKummy@users.noreply.github.com>
2024-03-19 13:10:23 +0800

    Test

--- Blockchain/TestChain.java
+++ Blockchain/TestChain.java
@@ -5,11 +5,33 @@ import com.google.gson.GsonBuilder;
 public class TestChain {
 
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
+    public static int difficulty = 1;
 
+    public static void main(String[] args) {
+        //add our blocks to the blockchain ArrayList
+        blockchain.add(new Block("This first block is for testing", "0"));
+        System.out.println("Tyring to mine block 1...");
+        blockchain.get(0).mineBlock(difficulty);
+
+        blockchain.add(new Block("Yo second block here", blockchain.get(blockchain.size() - 1).hash));
+        System.out.println("Trying to mine block 2...");
+        blockchain.get(1).mineBlock(difficulty);
+
+        blockchain.add(new Block("Hey Im the thrid one", blockchain.get(blockchain.size() - 1).hash));
+        System.out.println("Trying to mine block 3...");
+        blockchain.get(2).mineBlock(difficulty);
+
+        System.out.println("\nBlockChain is Valid: " + isChainValid());
+
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println("\nThe block chain: ");;
+        System.out.println(blockchainJson);
+    }
 
     public static Boolean isChainValid(){
         Block currentBlock;
         Block previousBlock;
+        String hashTarget = new String(new char[difficulty]).replace('\0', '0');
 
         //loop through blockchain to check hashes
         for(int i = 1; i < blockchain.size(); i++){
@@ -24,16 +46,12 @@ public class TestChain {
                 System.out.println("Previous Hashes not equal");
                 return false;
             }
+            //check if hash is solved
+            if (!currentBlock.hash.substring(0, difficulty).equals(hashTarget)) {
+                System.out.println("This block hasn't been mined");
+                return false;
+            }
         }
         return true;
-    }
-    public static void main(String[] args) {
-        //add our blocks to the blockchain ArrayList
-        blockchain.add(new Block("This first block is for testing", "0"));
-        blockchain.add(new Block("Yo second block here", blockchain.get(blockchain.size() - 1).hash));
-        blockchain.add(new Block("Hey Im the thrid one", blockchain.get(blockchain.size() - 1).hash));
-        
-        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
-        System.out.println(blockchainJson);
     }
 }
