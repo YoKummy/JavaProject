f85b5709ca010db6e1c358fad55004d6b7032332
YoKummy <80161555+YoKummy@users.noreply.github.com>
2024-03-22 16:15:12 +0800

    Final

--- Blockchain/Block.java
+++ Blockchain/Block.java
@@ -4,13 +4,13 @@ public class Block {
     
     public String hash;//hold digital signature
     public String previousHash;
-    private String data;//block data
+    public String merkleRoot;
+    public ArrayList<Transaction> transactions = new ArrayList<Transaction>();
     private long timeStamp;//as number of millisecinds since 1/1/1970
     private int nonce;
 
     //Block Constructor
-    public Block(String data, String previousHash){
-        this.data = data;
+    public Block(String previousHash){
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
         this.hash = calculateHash();//Put this in the end after we set all of the values
@@ -18,17 +18,32 @@ public class Block {
     
     public String calculateHash(){
         String calculatehash = StringUtil.applySha256(
-            previousHash + Long.toString(timeStamp) + Integer.toString(nonce) +data
+            previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + merkleRoot
         );
         return calculatehash;
     }
     
     public void mineBlock(int difficulty){
-        String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
+        merkleRoot = StringUtil.getMerkleRoot(transactions);
+        String target = StringUtil.getDificultyString(difficulty); //Create a string with difficulty * "0"
         while (!hash.substring(0, difficulty).equals(target)) {
             nonce ++;
             hash = calculateHash();
         }
         System.out.println("Block Mined!" + hash);
+    }
+
+    public boolean addTransaction(Transaction transaction){
+        //process transaction and check if valid, unless block is genesis block then ignore
+        if(transaction == null) return false;
+        if(previousHash != "0"){
+            if(transaction.processTransaction() != true){
+                System.out.println("Transaction failed to process. Discarded.");
+                return false;
+            }
+        }
+        transactions.add(transaction);
+        System.out.println("Transaction successfully added to block");
+        return true;
     }
 }
--- Blockchain/StringUtil.java
+++ Blockchain/StringUtil.java
@@ -1,9 +1,13 @@
 package Blockchain;
 
 import java.security.*;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 import org.bouncycastle.util.Exceptions;
+
+import com.google.gson.GsonBuilder;
 public class StringUtil {
     //Using Sha256 to string and returns the result
     public static String applySha256(String input){
@@ -56,7 +60,37 @@ public class StringUtil {
         }
     }
 
+    public static String getJson(Object o) {
+		return new GsonBuilder().setPrettyPrinting().create().toJson(o);
+	}
+
+    public static String getDificultyString(int difficulty) {
+		return new String(new char[difficulty]).replace('\0', '0');
+	}
+
     public static String getStringFromKey(Key key){
         return Base64.getEncoder().encodeToString(key.getEncoded());
     }
+
+    public static String getMerkleRoot(ArrayList<Transaction> transactions) {
+		int count = transactions.size();
+		
+		List<String> previousTreeLayer = new ArrayList<String>();
+		for(Transaction transaction : transactions) {
+			previousTreeLayer.add(transaction.transactionId);
+		}
+		List<String> treeLayer = previousTreeLayer;
+		
+		while(count > 1) {
+			treeLayer = new ArrayList<String>();
+			for(int i=1; i < previousTreeLayer.size(); i+=2) {
+				treeLayer.add(applySha256(previousTreeLayer.get(i-1) + previousTreeLayer.get(i)));
+			}
+			count = treeLayer.size();
+			previousTreeLayer = treeLayer;
+		}
+		
+		String merkleRoot = (treeLayer.size() == 1) ? treeLayer.get(0) : "";
+		return merkleRoot;
+	}
 }
